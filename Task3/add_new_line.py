022d7183ad5f2b833d95c55ebbcfa0c9c2976a06
Ojasvi Jalal <s1612970@sms.ed.ac.uk>
2019-10-29 21:00:24 +0000

    deleted

--- Task3/add_new_line.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python2.7
-# mapper.py
-
-import sys
-for line in sys.stdin:
-    print(line.strip())
--- Task4/mapper2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python2.7
-# mapper.py
-# cat reducer_input.xdfnghxbxdflhkgndkljgncdfkl | ./reducer.py
-
-import sys
-
-for line in sys.stdin:
-    print(line.strip())
